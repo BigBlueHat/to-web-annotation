bf351b06fd71b156d06f18199ed5afe8dafe17ce
BigBlueHat <byoung@bigbluehat.com>
2015-10-24 22:56:20 -0400

    Factor `to` function to from-annotator

--- src/from-annotator/index.js
+++ src/from-annotator/index.js
@@ -35,5 +35,41 @@ module.exports = {
       "--original--": annotation,
       "@context": context
     };
+  },
+  /**
+   * From Web Annotation to http://annotatorjs.org/
+   *
+   * TODO: give this a better function name?
+   **/
+  to: function(annotation) {
+    if (undefined === annotation['@type']
+        || annotation['@type'] !== 'oa:Annotation'
+        // TODO: maybe check the @context to be sure we mean `oa:Annotation`?
+        || annotation['@type'] !== 'Annotation') {
+      // if it's not a Web Annotation, don't change it
+      return annotation;
+    }
+    var rv = {
+      "uri": annotation.target.source,
+      "quote": "",
+      "text": annotation.body,
+      "ranges": [{}]
+    };
+
+    var selectors = annotation.target.selector.members;
+    for (var i = 0; i < selectors.length; i++) {
+      if (selectors[i]['@type'] === 'oa:TextQuoteSelector') {
+        rv.quote = selectors[i].exact;
+      } else if (selectors[i]['@type'] === 'oa:TextPositionSelector') {
+        rv.ranges[0].startOffset = selectors[i].start;
+        rv.ranges[0].endOffset = selectors[i].end;
+      }
+    }
+
+    // TODO: handle multiple ranges
+    rv.ranges[0].start = annotation['--original--'].ranges[0].start;
+    rv.ranges[0].end = annotation['--original--'].ranges[0].end;
+
+    return rv;
   }
 };
--- src/index.js
+++ src/index.js
@@ -5,33 +5,6 @@ var context = require('./context.json');
 var fromAnnotatorToWebAnnotation = require('./from-annotator').from;
 
 var fromHypothesisToWebAnnotation = require('./from-hypothesis').from;
-/**
- * From http://hypothes.is/ JSON to Web Annotation
- **/
-function fromHypothesisToWebAnnotation(annotation) {
-  // TODO: remove non-JSON-LD cruft? or leave it?
-  //
-  // TODO: "upgrade" tags & text to separate bodies with roles
-  // TODO: turn RangeSelector (xpath) into #xpointer FragmentSelector
-  var creator_nick = annotation.user.replace('acct:', '')
-    .replace('@hypothes.is', '');
-  return {
-    "@id": "http://hypothes.is/a/" + annotation.id,
-    "@type": "oa:Annotation",
-    "creator": {
-      "@id": annotation.user,
-      "@type": "Person",
-      "nick": creator_nick
-    },
-    "body": annotation.text,
-    "created": annotation.created,
-    "target": annotation.target,
-    // TODO: where should we keep the xpath stuff in Web Annotation?
-    // ...this key is ugly on purpose...
-    "--original--": annotation,
-    "@context": context
-  };
-}
 
 function toWebAnnotation(annotation) {
   if (undefined !== annotation.id
@@ -53,35 +26,9 @@ function toWebAnnotation(annotation) {
 }
 
 function fromWebAnnotation(annotation) {
-  if (undefined === annotation['@type']
-      || annotation['@type'] !== 'oa:Annotation'
-      // TODO: maybe check the @context to be sure we mean `oa:Annotation`?
-      || annotation['@type'] !== 'Annotation') {
-    // if it's not a Web Annotation, don't change it
-    return annotation;
-  }
-  var rv = {
-    "uri": annotation.target.source,
-    "quote": "",
-    "text": annotation.body,
-    "ranges": [{}]
-  };
-
-  var selectors = annotation.target.selector.members;
-  for (var i = 0; i < selectors.length; i++) {
-    if (selectors[i]['@type'] === 'oa:TextQuoteSelector') {
-      rv.quote = selectors[i].exact;
-    } else if (selectors[i]['@type'] === 'oa:TextPositionSelector') {
-      rv.ranges[0].startOffset = selectors[i].start;
-      rv.ranges[0].endOffset = selectors[i].end;
-    }
-  }
-
-  // TODO: handle multiple ranges
-  rv.ranges[0].start = annotation['--original--'].ranges[0].start;
-  rv.ranges[0].end = annotation['--original--'].ranges[0].end;
-
-  return rv;
+  // TODO: support more than just Annotator.js
+  // TODO: ...yeah...confusing function name
+  return require('./from-annotator').to;
 }
 
 exports.toWebAnnotation = toWebAnnotation;
